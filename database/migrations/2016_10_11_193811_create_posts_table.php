9f7d13a5c47210adda21d59bcc39c42e979b063a
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-10-14 08:25:15 -0500

    Update:
    Not sure if I've pushed since the initial commit.

---
+++ database/migrations/2016_10_11_193811_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            // with our foregin keys we want to create the column name before assigning them as foreign key 
+            $table->integer('created_by')->unsigned();
+            $table->foreign('created_by')->references('id')->on('users');
+            
+            $table->string('title');
+            $table->string('url')->nullable();
+            $table->text('content');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
