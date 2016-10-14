8b592f53ae842ddcf9c43cd959e9e182daba5641
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-10-13 21:04:15 -0500

    Update: Not sure if I've pushed since Initial Commit.

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $users = User::all();
+        $data = ['users'=>$users];
+        return view('users.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    
+         return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = new Post;
+           $user->created_by = 1;
+           $user->title = $request->title;
+           $user->content = $request->content;
+           $user->url = $request->url;
+           $user->save();
+
+        return redirect()->action('UsersController@show', $user->$id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+        $data = ['user' => $user];
+        // dd($post);
+        return view('users.show')->with($data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        $data = ['user' => $user];
+        return view('user.edit')->with($data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->title = $request->title;
+        $user->content = $request->content;
+        $user->url = $request->url;
+        $user->save();
+
+        return redirect()->action('UsersController@show', $user->$id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
