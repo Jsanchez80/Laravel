f5791eee90be426c933ea915a72f0ca594959c7b
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-10-14 08:20:28 -0500

    Update:
    Not sure if I've pushed since the initial commit.

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -9,13 +9,13 @@
 	
 	<h1>Please edit your Information: </h1>
 	{{-- Avoid a CSRf Attach using CSRF protection "{!! csrf_field() !!}" --}}
-	<form method="POST" action="{{ action('PostsController@update', 1) }}">
+	<form method="POST" action="{{ action('PostsController@update', $post->id) }}">
 		{!! csrf_field() !!}
 		{{-- calling the {!! method_field('PUT') !!}  ensures that you're entries are fed to the database and updated correctly. A form of method spoofing --}}
 		{!! method_field('PUT') !!}
-		Title: <input type="text" name="title" value="{{ old('title') }}">
-		Content: <input type="text" name="content" value="{{ old('content') }}">
-		URL: <input type="text" name="url" value="{{ old('url') }}">
+		Title: <input type="text" name="title" value="{{ old('title') == null ? $post->title : old('title') }}">
+		Content: <input type="text" name="content" value="{{ old('content') == null ? $post->content : old('content') }}">
+		URL: <input type="text" name="url" value="{{ old('url') == null ? $post->url : old('url') }}">
 		<input type="submit">
 	</form>
 
