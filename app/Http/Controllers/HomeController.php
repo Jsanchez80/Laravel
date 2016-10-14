d48eaaf9fa0ff2f04331c6415ea955cb54840d9e
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-10-13 21:09:46 -0500

    Update: Not sure if I've pushed since initial commit.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,21 +4,50 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
-    public function showWelcome()
-    {
-        return view('welcome');
-    }
+    // public function showWelcome()
+    // {
+    //     return view('welcome');
+    // }
 
-    public function rollDice($guess)
-    {
-    	$numberOfRolls = 6;
-    	$rolls = [];
-    	for ($i = 0; $i < $numberOfRolls; $i++);
-    			$frolls[] = mt_rand(1,6);
-    }
-    return view('dice'->with([
-    	'rolls' => $rolls,
-    	'guess' => $guess,
+ //    public function rollDice($guess)
+ //    {
+ //    	$numberOfRolls = 6;
+ //    	$rolls = [];
+ //    	for ($i = 0; $i < $numberOfRolls; $i++);
+ //    			$frolls[] = mt_rand(1,6);
+ //    }
+    
+ //    return view('dice'->with([
+ //    	'rolls' => $rolls,
+ //    	'guess' => $guess,
     	
-    	]));
+ //    	]));
+
+	// }
+
+	// public function uppercase($word)
+	// {
+	// 	return  strtoupper("$word");
+	// }
+
+	// public function increment($number = 1) {
+	// return  $number + 1;
+	// }
+
+
+
+	public function rolldice()
+	{
+
+	$die1 = mt_rand(1,6) . PHP_EOL;
+	$die2 = mt_rand(1,6) . PHP_EOL;
+	$data['die1'] = $die1;
+	$data['die2'] = $die2;
+	// echo $die1;
+	// echo $die
+	// var_dump($die1);
+	// var_dump($die2);
+	// echo $die1 + $die2;
+	return view('exercises.rolldice2')->with($data);
+	}
 }
