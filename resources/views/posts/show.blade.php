8a1deae0801f40968696fa27a7f8d9f0ca162250
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-10-20 09:46:52 -0500

    update: Debugging

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,21 +6,26 @@
 
 @section('content')
 	
-<div><h2 class="add">Place for your ad!</h2>
+<div><h2 class="add">Check out your post!</h2>
     <div class="row">
      <div class="col-md-10 blogShort">
 	     
+	    {{-- <h3>{{ $post->user->name }}</h3> --}}
 		<h3>{{ $post->title }} </h3>
 	    <p>{{ $post->content }}</p>
 	    <h3>{{ $post->url }}</h3>
+	    <h6>You originally created your post on {{ $post->created_at }}</h6>
+	    <h6>and updated your post on {{ $post->updated_at }}</h6>
+	    <p>User: {{ $post->user->name}}</p>
+	   
 	    <br>
 	     
 	  </div>    
     </div>    
 	    
-{{-- <div class="art-footer-text">
+<div class="art-footer-text">
     <p>EVOQ | Copyright © 2016</p>
-</div> --}}
+</div>
 
 
 @stop
