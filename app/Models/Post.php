19a2cabd05c3d6685aea8c3f1e7902f97e68fcf2
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-10-20 09:21:27 -0500

    Update:
    Made some changes in the Posts.php to ensure posts are formatted correctly.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Post extends BaseModel
 {
-    //
+    // public function user tells laravel that the public function posts located in User.php is relational/dependent on it. 
+    public function user()
+    	{
+    		return $this->belongsTo('App\User', 'created_by', 'id');
+    	}
+
+    
 }
