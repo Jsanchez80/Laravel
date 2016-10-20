1083efe95cf90acd4e31b6a90ab0e482acb1d722
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-10-20 09:45:48 -0500

    Update: Debugging

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,13 +6,25 @@
 
 @section('content')
 	
-	<h1>Please create your file: </h1>
-	<form method="POST" action="{{ action('PostsController@store') }}">
+	<h1>Please create your post: </h1>
+{{-- <div class="container" style=" border-radius: 5px; border: 1px solid black;
+    border-collapse: collapse; padding: 30px"> --}}
+	<form method="POST" style=" border-radius: 5px; border: 1px solid black;
+    border-collapse: collapse; padding: 30px" action="{{ action('PostsController@store') }}">
 		{!! csrf_field() !!}
-		Title: <input type="text" name="title" value="{{ old('name') }}">
-		Content: <input type="text" name="content" value="{{ old('name') }}">
-		URL: <input type="text" name="url" value="{{ old('name') }}">
+		<div class="form-group">
+			<label for="inputTitle" type="text" name="title">Title:</label>
+			<input type="text" name="title" class="form-control" value="{{ old('name') }}">
+		</div>
+		<div class="form-group">
+			<label for="inputContent" type="text" name="content">Content:</label> 
+			<input type="text" name="content" class="form-control" value="{{ old('name') }}">
+		</div>
+		<div class="form-group">
+			<label for="inputURL" type="text" name="url">URL:</label>
+			<input type="text" name="url" class="form-control" value="{{ old('name') }}">
+		</div>
 		<input type="submit">
 	</form>
-
+{{-- </div> --}}
 @stop
